841cc7f0619362a80d6c040381f263fb396db71e
notzyron <notzyron@gmail.com>
2026-09-15 10:27:27 +0200

    Add CreateSoundCues method to EmoteAssetCreator, add cooked 8.51-9.10 .uasset and .uexp files for emote sound cues and emote 3P sound cues

--- UFMT/FnAssetsLogic/EmoteAssetCreator.cs
+++ UFMT/FnAssetsLogic/EmoteAssetCreator.cs
@@ -27,7 +27,6 @@ namespace UFMT.FnAssets
             foreach (DirectoryInfo subFolder in cookedEmoteDirectory.GetDirectories("*", SearchOption.AllDirectories))
             {
                 string targetSubDir = subFolder.FullName.Replace(cookedEmoteDirectory.FullName, contentFolderPath);
-                Log.Test($"target sub dir is {targetSubDir}");
                 Directory.CreateDirectory(targetSubDir);
 
                 foreach (FileInfo file in subFolder.GetFiles())
@@ -42,78 +41,126 @@ namespace UFMT.FnAssets
         internal static void CreateAnimationMontage(string OutputFnGameCurrentEmotePath, string animationName, float animationLength,
         FnVersion fnVersion, UeVersion ueVersion, string ueEmotesPackagePath, string codename)
         {
-            try 
-            {
-                string montageName = $"{animationName}_M.uasset";
-                string animationMontageUassetPath = Path.Combine(OutputFnGameCurrentEmotePath, "Animations", montageName);
+            string montageName = $"{animationName}_M.uasset";
+            string animationMontageUassetPath = Path.Combine(OutputFnGameCurrentEmotePath, "Animations", montageName);
 
-                byte[] montageUassetBase64 = TemplateLoader.GetEmbeddedFile(fnVersion.Name, "CookedUeAssets", "EmoteMontage.uasset");
-                byte[] montageUexpBase64 = TemplateLoader.GetEmbeddedFile(fnVersion.Name, "CookedUeAssets", "EmoteMontage.uexp");
+            byte[] montageUassetBase64 = TemplateLoader.GetEmbeddedFile(fnVersion.Name, "CookedUeAssets", "EmoteMontage.uasset");
+            byte[] montageUexpBase64 = TemplateLoader.GetEmbeddedFile(fnVersion.Name, "CookedUeAssets", "EmoteMontage.uexp");
 
 
-                File.WriteAllBytes(animationMontageUassetPath, montageUassetBase64);
-                File.WriteAllBytes(Path.ChangeExtension(animationMontageUassetPath, ".uexp"), montageUexpBase64);
+            File.WriteAllBytes(animationMontageUassetPath, montageUassetBase64);
+            File.WriteAllBytes(Path.ChangeExtension(animationMontageUassetPath, ".uexp"), montageUexpBase64);
 
-                var animationMontageAsset = new UAsset(animationMontageUassetPath, ueVersion.UassetApiEngineVer);
-                var exportData = animationMontageAsset.Exports;
-                var export0 = (NormalExport)exportData[0];
+            var animationMontageAsset = new UAsset(animationMontageUassetPath, ueVersion.UassetApiEngineVer);
+            var exportData = animationMontageAsset.Exports;
+            var export0 = (NormalExport)exportData[0];
 
-                export0.ObjectName.Value.Value = Path.GetFileNameWithoutExtension(animationMontageUassetPath);
+            export0.ObjectName.Value.Value = Path.GetFileNameWithoutExtension(animationMontageUassetPath);
 
-                var SequenceLength = (FloatPropertyData)export0["SequenceLength"];
-                SequenceLength.Value = animationLength;
+            var sequenceLength = (FloatPropertyData)export0["SequenceLength"];
+            sequenceLength.Value = animationLength;
 
-                var compositeSections = (ArrayPropertyData)export0["CompositeSections"];
-                var defaultCompositeSection = (StructPropertyData)compositeSections.Value[0];
-                var loopCompositeSection = (StructPropertyData)compositeSections.Value[1];
-                var defaultCompositeSectionSegmentLength = (FloatPropertyData)defaultCompositeSection["SegmentLength"];
-                var loopCompositeSectionSegmentLength = (FloatPropertyData)loopCompositeSection["SegmentLength"];
-                defaultCompositeSectionSegmentLength.Value = animationLength;
-                loopCompositeSectionSegmentLength.Value = animationLength;
+            var compositeSections = (ArrayPropertyData)export0["CompositeSections"];
+            var defaultCompositeSection = (StructPropertyData)compositeSections.Value[0];
+            var loopCompositeSection = (StructPropertyData)compositeSections.Value[1];
+            var defaultCompositeSectionSegmentLength = (FloatPropertyData)defaultCompositeSection["SegmentLength"];
+            var loopCompositeSectionSegmentLength = (FloatPropertyData)loopCompositeSection["SegmentLength"];
+            defaultCompositeSectionSegmentLength.Value = animationLength;
+            loopCompositeSectionSegmentLength.Value = animationLength;
 
-                var slotAnimTracks = (ArrayPropertyData)export0["SlotAnimTracks"];
-                var slotAnimTracks2 = (StructPropertyData)slotAnimTracks.Value[0];
-                var animTrack = (StructPropertyData)slotAnimTracks2.Value[1];
-                var animSegments = (ArrayPropertyData)animTrack.Value[0];
-                var animSegments2 = (StructPropertyData)animSegments.Value[0];
-                var animEndTime = (FloatPropertyData)animSegments2["AnimEndTime"];
-                animEndTime.Value = animationLength;
+            var slotAnimTracks = (ArrayPropertyData)export0["SlotAnimTracks"];
+            var slotAnimTracks2 = (StructPropertyData)slotAnimTracks.Value[0];
+            var animTrack = (StructPropertyData)slotAnimTracks2.Value[1];
+            var animSegments = (ArrayPropertyData)animTrack.Value[0];
+            var animSegments2 = (StructPropertyData)animSegments.Value[0];
+            var animEndTime = (FloatPropertyData)animSegments2["AnimEndTime"];
+            animEndTime.Value = animationLength;
 
-                var notifies = (ArrayPropertyData)export0["Notifies"];
+            var notifies = (ArrayPropertyData)export0["Notifies"];
 
-                var emoteSoundNotify = (StructPropertyData)notifies.Value[0];
-                var emoteSoundNotifyDuration = (FloatPropertyData)emoteSoundNotify["Duration"];
-                var emoteSoundNotifySegmentLength = (FloatPropertyData)emoteSoundNotify["SegmentLength"];
-                emoteSoundNotifyDuration.Value = animationLength;
-                emoteSoundNotifySegmentLength.Value = animationLength;
-                var emoteSoundNotifyEndLink = (StructPropertyData)emoteSoundNotify["EndLink"];
-                var emoteSoundNotifyEndLinkSegmentLength = (FloatPropertyData)emoteSoundNotifyEndLink["SegmentLength"];
-                var emoteSoundNotifyEndLinkLinkValue = (FloatPropertyData)emoteSoundNotifyEndLink["LinkValue"];
-                emoteSoundNotifyEndLinkSegmentLength.Value = animationLength;
-                emoteSoundNotifyEndLinkLinkValue.Value = animationLength;
+            var emoteSoundNotify = (StructPropertyData)notifies.Value[0];
+            var emoteSoundNotifyDuration = (FloatPropertyData)emoteSoundNotify["Duration"];
+            var emoteSoundNotifySegmentLength = (FloatPropertyData)emoteSoundNotify["SegmentLength"];
+            emoteSoundNotifyDuration.Value = animationLength;
+            emoteSoundNotifySegmentLength.Value = animationLength;
+            var emoteSoundNotifyEndLink = (StructPropertyData)emoteSoundNotify["EndLink"];
+            var emoteSoundNotifyEndLinkSegmentLength = (FloatPropertyData)emoteSoundNotifyEndLink["SegmentLength"];
+            var emoteSoundNotifyEndLinkLinkValue = (FloatPropertyData)emoteSoundNotifyEndLink["LinkValue"];
+            emoteSoundNotifyEndLinkSegmentLength.Value = animationLength;
+            emoteSoundNotifyEndLinkLinkValue.Value = animationLength;
 
-                var holsterWeaponNotify = (StructPropertyData)notifies.Value[1];
-                var holsterWeaponNotifyDuration = (FloatPropertyData)holsterWeaponNotify["Duration"];
-                var holsterWeaponNotifySegmentLength = (FloatPropertyData)holsterWeaponNotify["SegmentLength"];
-                holsterWeaponNotifyDuration.Value = animationLength;
-                holsterWeaponNotifySegmentLength.Value = animationLength;
-                var holsterWeaponNotifyEndLink = (StructPropertyData)holsterWeaponNotify["EndLink"];
-                var holsterWeaponNotifyEndLinkSegmentLength = (FloatPropertyData)holsterWeaponNotifyEndLink["SegmentLength"];
-                var holsterWeaponNotifyEndLinkLinkValue = (FloatPropertyData)holsterWeaponNotifyEndLink["LinkValue"];
-                holsterWeaponNotifyEndLinkSegmentLength.Value = animationLength;
-                holsterWeaponNotifyEndLinkLinkValue.Value = animationLength;
+            var holsterWeaponNotify = (StructPropertyData)notifies.Value[1];
+            var holsterWeaponNotifyDuration = (FloatPropertyData)holsterWeaponNotify["Duration"];
+            var holsterWeaponNotifySegmentLength = (FloatPropertyData)holsterWeaponNotify["SegmentLength"];
+            holsterWeaponNotifyDuration.Value = animationLength;
+            holsterWeaponNotifySegmentLength.Value = animationLength;
+            var holsterWeaponNotifyEndLink = (StructPropertyData)holsterWeaponNotify["EndLink"];
+            var holsterWeaponNotifyEndLinkSegmentLength = (FloatPropertyData)holsterWeaponNotifyEndLink["SegmentLength"];
+            var holsterWeaponNotifyEndLinkLinkValue = (FloatPropertyData)holsterWeaponNotifyEndLink["LinkValue"];
+            holsterWeaponNotifyEndLinkSegmentLength.Value = animationLength;
+            holsterWeaponNotifyEndLinkLinkValue.Value = animationLength;
 
-                var importData = animationMontageAsset.Imports;
-                importData[2].ObjectName.Value.Value = montageName;
-                importData[9].ObjectName.Value.Value = $"{ueEmotesPackagePath}/{codename}/{animationName}";
+            var importData = animationMontageAsset.Imports;
+            importData[2].ObjectName.Value.Value = montageName;
+            importData[9].ObjectName.Value.Value = $"{ueEmotesPackagePath}/{codename}/{animationName}";
 
-                animationMontageAsset.Write(animationMontageUassetPath);
-                Log.Success($"Succesfully edited {Path.GetFileNameWithoutExtension(animationMontageUassetPath)}");
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex.ToString());
-            }
+            animationMontageAsset.Write(animationMontageUassetPath);
+            Log.Success($"Succesfully edited {Path.GetFileNameWithoutExtension(animationMontageUassetPath)}");
+        }
+
+        internal static void CreateSoundCues(string OutputFnGameCurrentEmotePath, FnVersion fnVersion, UeVersion ueVersion, string ueEmotesPackagePath, string codename)
+        {
+            string soundCueName = $"SC_EmoteMusic_{codename}.uasset";
+            string soundCue3PName = $"SC_EmoteMusic3P_{codename}.uasset";
+            string soundCueUassetPath = Path.Combine(OutputFnGameCurrentEmotePath, "Sound", soundCueName);
+            string soundCue3PUassetPath = Path.Combine(OutputFnGameCurrentEmotePath, "Sound", soundCue3PName);
+
+            byte[] soundCueUassetBase64 = TemplateLoader.GetEmbeddedFile(fnVersion.Name, "CookedUeAssets", "EmoteSoundCue.uasset");
+            byte[] soundCueUexpBase64 = TemplateLoader.GetEmbeddedFile(fnVersion.Name, "CookedUeAssets", "EmoteSoundCue.uexp");
+            byte[] soundCue3PUassetBase64 = TemplateLoader.GetEmbeddedFile(fnVersion.Name, "CookedUeAssets", "EmoteSoundCue3P.uasset");
+            byte[] soundCue3PUexpBase64 = TemplateLoader.GetEmbeddedFile(fnVersion.Name, "CookedUeAssets", "EmoteSoundCue3P.uexp");
+
+
+            File.WriteAllBytes(soundCueUassetPath, soundCueUassetBase64);
+            File.WriteAllBytes(Path.ChangeExtension(soundCueUassetPath, ".uexp"), soundCueUexpBase64);
+            File.WriteAllBytes(soundCue3PUassetPath, soundCue3PUassetBase64);
+            File.WriteAllBytes(Path.ChangeExtension(soundCue3PUassetPath, ".uexp"), soundCue3PUexpBase64);
+
+            float soundWaveLength =
+            ((FloatPropertyData)((NormalExport)new UAsset(Path.Combine(OutputFnGameCurrentEmotePath, "Sound", $"{codename}_Sound.uasset"), ueVersion.UassetApiEngineVer).Exports[0])["Duration"]).Value;
+
+            var soundCueAsset = new UAsset(soundCueUassetPath, ueVersion.UassetApiEngineVer);
+            var exportData = soundCueAsset.Exports;
+            var export0 = (NormalExport)exportData[0];
+
+            export0.ObjectName.Value.Value = Path.GetFileNameWithoutExtension(soundCueName);
+            var duration = (FloatPropertyData)export0["Duration"];
+            duration.Value = soundWaveLength;
+
+            var export1 = (NormalExport)exportData[1];
+            var soundWaveAssetPtr = (SoftObjectPropertyData)export1["SoundWaveAssetPtr"];
+            soundWaveAssetPtr.Value.AssetPath.AssetName.Value.Value = $"{ueEmotesPackagePath}/{codename}/{codename}_Sound.{codename}_Sound";
+
+            var importData = soundCueAsset.Imports;
+            importData[4].ObjectName.Value.Value = $"{ueEmotesPackagePath}/{codename}/{codename}_Sound";
+            importData[10].ObjectName.Value.Value = $"{codename}_Sound";
+            soundCueAsset.Write(soundCueUassetPath);
+
+            var soundCue3PAsset = new UAsset(soundCue3PUassetPath, ueVersion.UassetApiEngineVer);
+            exportData = soundCue3PAsset.Exports;
+            export0 = (NormalExport)exportData[0];
+            export0.ObjectName.Value.Value = Path.GetFileNameWithoutExtension(soundCue3PName);
+            duration = (FloatPropertyData)export0["Duration"];
+            duration.Value = soundWaveLength;
+
+            export1 = (NormalExport)exportData[1];
+            soundWaveAssetPtr = (SoftObjectPropertyData)export1["SoundWaveAssetPtr"];
+            soundWaveAssetPtr.Value.AssetPath.AssetName.Value.Value = $"{ueEmotesPackagePath}/{codename}/{codename}_Sound.{codename}_Sound";
+
+            importData = soundCue3PAsset.Imports;
+            importData[5].ObjectName.Value.Value = $"{ueEmotesPackagePath}/{codename}/{codename}_Sound";
+            importData[12].ObjectName.Value.Value = $"{codename}_Sound";
+            soundCue3PAsset.Write(soundCue3PUassetPath);
         }
     }
 }
