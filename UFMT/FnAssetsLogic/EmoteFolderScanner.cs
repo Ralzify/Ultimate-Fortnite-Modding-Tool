5bd2151d31edd9b716e4ab45b847ed62f1db3abf
notzyron <notzyron@gmail.com>
2026-09-15 14:40:13 +0200

    Add .json searching for animations in EmoteFolderScanner

--- UFMT/FnAssetsLogic/EmoteFolderScanner.cs
+++ UFMT/FnAssetsLogic/EmoteFolderScanner.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 using UFMT.Core;
 using UFMT.FnAssets;
@@ -12,7 +13,8 @@ namespace UFMT.FnAssetsLogic
 {
     internal class EmoteFolderScanner
     {
-        internal static (bool success, string maleAnimationPsaFileName, string femaleAnimationPsaFileName) GetAnimationsData(string animationsFolderPath)
+        internal static (bool success, string maleAnimationPsaFileName, string femaleAnimationPsaFileName, string maleAnimationJsonFileName, 
+        string femaleAnimationJsonFileName)GetAnimationsData(string animationsFolderPath)
         {
             string maleAnimationFolderPath = Path.Combine(animationsFolderPath, "Male");
             string femaleAnimationFolderPath = Path.Combine(animationsFolderPath, "Female");
@@ -23,19 +25,19 @@ namespace UFMT.FnAssetsLogic
             if (maleAnimationPsaFilePaths.Length > 1) 
             {
                 Log.Error($"Multiple .psa files found in \"{maleAnimationFolderPath}\"!");
-                return (false, null, null);
+                return (false, null, null, null, null);
             }
 
             if (femaleAnimationPsaFilePaths.Length > 1)
             {
                 Log.Error($"Multiple .psa files found in \"{femaleAnimationFolderPath}\"!");
-                return (false, null, null);
+                return (false, null, null, null, null);
             }
 
             if (maleAnimationPsaFilePaths.Length == 0 && femaleAnimationPsaFilePaths.Length == 0)
             {
                 Log.Error("No animations found for the emote (no .psa files found in Male or Female animation folders)!");
-                return (false, null, null);
+                return (false, null, null, null, null);
             }
 
             if (maleAnimationPsaFilePaths.Length == 0) Log.Warning($"No .psa files found in \"{maleAnimationFolderPath}\"");
@@ -43,7 +45,38 @@ namespace UFMT.FnAssetsLogic
             Log.Success($"Found {Path.GetFileName(maleAnimationPsaFilePaths[0])} in Male animation folder");
             Log.Success($"Found {Path.GetFileName(femaleAnimationPsaFilePaths[0])} in Female animation folder");
 
-            return (true, Path.GetFileName(maleAnimationPsaFilePaths[0]), Path.GetFileName(femaleAnimationPsaFilePaths[0]));
+            string[] maleAnimationJsonFilePaths = Directory.GetFiles(maleAnimationFolderPath, "*.json");
+            string[] femaleAnimationJsonFilePaths = Directory.GetFiles(femaleAnimationFolderPath, "*.json");
+
+            if (maleAnimationJsonFilePaths.Length > 1)
+            {
+                Log.Error($"Multiple .json files in \"{maleAnimationFolderPath}\"!\nMake sure there is only 1 .json animation!");
+                return (false, null, null, null, null);
+            }
+            if (maleAnimationJsonFilePaths.Length != 0)
+            {
+                Log.Success($"Found {Path.GetFileName(maleAnimationJsonFilePaths[0])} in Male animation folder");
+            }
+            else
+            {
+                maleAnimationJsonFilePaths = [string.Empty];
+            }
+            if (femaleAnimationJsonFilePaths.Length > 1)
+            {
+                Log.Error($"Multiple .json files in \"{femaleAnimationFolderPath}\"!\nMake sure there is only 1 .json animation!");
+                return (false, null, null, null, null);
+            }
+            if (femaleAnimationJsonFilePaths.Length != 0)
+            {
+                Log.Success($"Found {Path.GetFileName(femaleAnimationJsonFilePaths[0])} in Female animation folder");
+            }
+            else
+            {
+                femaleAnimationJsonFilePaths = [string.Empty];
+            }
+
+            return (true, Path.GetFileName(maleAnimationPsaFilePaths[0]), Path.GetFileName(femaleAnimationPsaFilePaths[0]), Path.GetFileName(maleAnimationJsonFilePaths[0]),
+            Path.GetFileName(femaleAnimationJsonFilePaths[0]));
         }
 
         internal static (bool, string) GetSoundData(string SoundFolderPath)
