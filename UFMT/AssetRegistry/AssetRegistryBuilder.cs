60fde7c139fbb546f52c90807ad1fa8fd7dbdf0a
notzyron <notzyron@gmail.com>
2026-09-16 15:12:03 +0200

    Add path checks for skin and emote directories in CreateAssetRegistry method to prevent crashing or infinite loading

--- UFMT/AssetRegistry/AssetRegistryBuilder.cs
+++ UFMT/AssetRegistry/AssetRegistryBuilder.cs
@@ -19,79 +19,87 @@ namespace UFMT.AssetRegistry
             string cookedSkinsPath = Path.Combine(cookedAssetsPath, ueSkinsPackagePath.Substring(6, ueSkinsPackagePath.Length - 6).Replace("/", "\\"));
             string cookedEmotesPath = Path.Combine(cookedAssetsPath, ueEmotesPackagePath.Substring(6, ueEmotesPackagePath.Length - 6).Replace("/", "\\"));
 
-            string[] customSkinFolders = Directory.GetDirectories(Path.Combine(cookedSkinsPath));
-            string[] customEmoteFolders = Directory.GetDirectories(Path.Combine(cookedEmotesPath));
+            string[] customSkinFolders = [];
+            if (Path.Exists(cookedSkinsPath)) customSkinFolders = Directory.GetDirectories(Path.Combine(cookedSkinsPath));
+            string[] customEmoteFolders = [];
+            if (Path.Exists(cookedEmotesPath)) customEmoteFolders = Directory.GetDirectories(Path.Combine(cookedEmotesPath));
             List<string> cosmeticJsons = new();
 
             Console.WriteLine($"Searching for CIDs inside {cookedAssetsPath}...");
-            foreach (string customSkinFolder in customSkinFolders)
+            if (customSkinFolders.Length != 0)
             {
-                string[] cid = Directory.GetFiles(customSkinFolder, "*.uasset");
-                if (cid.Length > 0)
+                foreach (string customSkinFolder in customSkinFolders)
                 {
-                    string currentFoundCid = Path.GetFileNameWithoutExtension(cid[0]);
-
-                    string json = System.Text.Encoding.UTF8.GetString(TemplateLoader.GetEmbeddedFile(ueVersionNumber, "RawUeAssets", "Cid.json"));
-                    var root = JObject.Parse(json);
-                    root["ObjectPath"] = root["ObjectPath"]!.Value<string>()!.Replace(
-                        $"/Game/Athena/Items/Cosmetics/Characters/CID_Template.CID_Template",
-                        $"/Game/Athena/Items/Cosmetics/Characters/{currentFoundCid}.{currentFoundCid}");
-                    root["PackageName"] = root["PackageName"]!.Value<string>()!.Replace(
-                        $"/Game/Athena/Items/Cosmetics/Characters/CID_Template",
-                        $"/Game/Athena/Items/Cosmetics/Characters/{currentFoundCid}");
-                    root["AssetName"] = root["PackageName"]!.Value<string>()!.Replace(
-                        "CID_Template", currentFoundCid);
-                    var tagAndValue = root["TagAndValue"]!.ToArray();
-                    foreach (var tag in tagAndValue)
+                    string[] cid = Directory.GetFiles(customSkinFolder, "*.uasset");
+                    if (cid.Length > 0)
                     {
-                        if (tag["Item1"]!.Value<string>() == "PrimaryAssetName")
+                        string currentFoundCid = Path.GetFileNameWithoutExtension(cid[0]);
+
+                        string json = System.Text.Encoding.UTF8.GetString(TemplateLoader.GetEmbeddedFile(ueVersionNumber, "RawUeAssets", "Cid.json"));
+                        var root = JObject.Parse(json);
+                        root["ObjectPath"] = root["ObjectPath"]!.Value<string>()!.Replace(
+                            $"/Game/Athena/Items/Cosmetics/Characters/CID_Template.CID_Template",
+                            $"/Game/Athena/Items/Cosmetics/Characters/{currentFoundCid}.{currentFoundCid}");
+                        root["PackageName"] = root["PackageName"]!.Value<string>()!.Replace(
+                            $"/Game/Athena/Items/Cosmetics/Characters/CID_Template",
+                            $"/Game/Athena/Items/Cosmetics/Characters/{currentFoundCid}");
+                        root["AssetName"] = root["PackageName"]!.Value<string>()!.Replace(
+                            "CID_Template", currentFoundCid);
+                        var tagAndValue = root["TagAndValue"]!.ToArray();
+                        foreach (var tag in tagAndValue)
                         {
-                            tag["Item2"] = currentFoundCid;
-                            break;
+                            if (tag["Item1"]!.Value<string>() == "PrimaryAssetName")
+                            {
+                                tag["Item2"] = currentFoundCid;
+                                break;
+                            }
                         }
+                        cosmeticJsons.Add(root.ToString(Formatting.Indented));
+                        Console.WriteLine($"Added {currentFoundCid} to the AssetRegistry.bin!");
                     }
-                    cosmeticJsons.Add(root.ToString(Formatting.Indented));
-                    Console.WriteLine($"Added {currentFoundCid} to the AssetRegistry.bin!");
-                }
-                else
-                {
-                    Log.Warning($"no uasset files found inside {customSkinFolder}");
+                    else
+                    {
+                        Log.Warning($"no uasset files found inside {customSkinFolder}");
+                    }
                 }
             }
 
             Console.WriteLine($"Searching for EIDs inside {cookedAssetsPath}...");
-            foreach (string customEmoteFolder in customEmoteFolders)
+            if (customEmoteFolders.Length != 0)
             {
-                string[] eid = Directory.GetFiles(customEmoteFolder, "*.uasset");
-                if (eid.Length > 0)
+                foreach (string customEmoteFolder in customEmoteFolders)
                 {
-                    string currentFoundEid = Path.GetFileNameWithoutExtension(eid[0]);
-
-                    string json = System.Text.Encoding.UTF8.GetString(TemplateLoader.GetEmbeddedFile(ueVersionNumber, "RawUeAssets", "Eid.json"));
-                    var root = JObject.Parse(json);
-                    root["ObjectPath"] = root["ObjectPath"]!.Value<string>()!.Replace(
-                        $"/Game/Athena/Items/Cosmetics/Dances/EID_Template.EID_Template",
-                        $"/Game/Athena/Items/Cosmetics/Dances/{currentFoundEid}.{currentFoundEid}");
-                    root["PackageName"] = root["PackageName"]!.Value<string>()!.Replace(
-                        $"/Game/Athena/Items/Cosmetics/Dances/EID_Template",
-                        $"/Game/Athena/Items/Cosmetics/Dances/{currentFoundEid}");
-                    root["AssetName"] = root["PackageName"]!.Value<string>()!.Replace(
-                        "EID_Template", currentFoundEid);
-                    var tagAndValue = root["TagAndValue"]!.ToArray();
-                    foreach (var tag in tagAndValue)
+                    string[] eid = Directory.GetFiles(customEmoteFolder, "*.uasset");
+                    if (eid.Length > 0)
                     {
-                        if (tag["Item1"]!.Value<string>() == "PrimaryAssetName")
+                        string currentFoundEid = Path.GetFileNameWithoutExtension(eid[0]);
+
+                        string json = System.Text.Encoding.UTF8.GetString(TemplateLoader.GetEmbeddedFile(ueVersionNumber, "RawUeAssets", "Eid.json"));
+                        var root = JObject.Parse(json);
+                        root["ObjectPath"] = root["ObjectPath"]!.Value<string>()!.Replace(
+                            $"/Game/Athena/Items/Cosmetics/Dances/EID_Template.EID_Template",
+                            $"/Game/Athena/Items/Cosmetics/Dances/{currentFoundEid}.{currentFoundEid}");
+                        root["PackageName"] = root["PackageName"]!.Value<string>()!.Replace(
+                            $"/Game/Athena/Items/Cosmetics/Dances/EID_Template",
+                            $"/Game/Athena/Items/Cosmetics/Dances/{currentFoundEid}");
+                        root["AssetName"] = root["PackageName"]!.Value<string>()!.Replace(
+                            "EID_Template", currentFoundEid);
+                        var tagAndValue = root["TagAndValue"]!.ToArray();
+                        foreach (var tag in tagAndValue)
                         {
-                            tag["Item2"] = currentFoundEid;
-                            break;
+                            if (tag["Item1"]!.Value<string>() == "PrimaryAssetName")
+                            {
+                                tag["Item2"] = currentFoundEid;
+                                break;
+                            }
                         }
+                        cosmeticJsons.Add(root.ToString(Formatting.Indented));
+                        Console.WriteLine($"Added {currentFoundEid} to the AssetRegistry.bin!");
                     }
-                    cosmeticJsons.Add(root.ToString(Formatting.Indented));
-                    Console.WriteLine($"Added {currentFoundEid} to the AssetRegistry.bin!");
-                }
-                else
-                {
-                    Log.Warning($"no uasset files found inside {customEmoteFolder}");
+                    else
+                    {
+                        Log.Warning($"no uasset files found inside {customEmoteFolder}");
+                    }
                 }
             }
 
